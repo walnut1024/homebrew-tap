be211d1bfaf65b4d7f2d2c5bb511a8c543f2a625
walnut1024 <walnut1024@163.com>
2026-07-13 23:07:47 +0800

    brew: update shareslices to 0.1.6

--- Formula/shareslices.rb
+++ Formula/shareslices.rb
@@ -1,17 +1,17 @@
 class Shareslices < Formula
   desc "Publish and manage ShareSlices artifacts"
   homepage "https://github.com/walnut1024/ShareSlices"
-  version "0.1.5"
+  version "0.1.6"
 
   on_macos do
     odie "ShareSlices supports macOS Apple Silicon only" unless Hardware::CPU.arm?
-    url "https://github.com/walnut1024/ShareSlices/releases/download/cli-v0.1.5/shareslices-aarch64-apple-darwin.tar.gz"
-    sha256 "54dbbb4aace0835228f92ee3b099edb649ea3c993e249bdb30038973b8f15e05"
+    url "https://github.com/walnut1024/ShareSlices/releases/download/cli-v0.1.6/shareslices-aarch64-apple-darwin.tar.gz"
+    sha256 "0a5c789877c2350af451297d1d8019380569c892ed5a2d63d882608edc3c4ccd"
   end
 
   on_linux do
-    url "https://github.com/walnut1024/ShareSlices/releases/download/cli-v0.1.5/shareslices-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5c6984cee3e8363b7b5dc81a38c073fb91054a61da673e2a007c80da7debac07"
+    url "https://github.com/walnut1024/ShareSlices/releases/download/cli-v0.1.6/shareslices-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "fd9a020bdf8c7025f83d157be2b52923b82520cbf9998230ea6c55160989b9e7"
   end
 
   def install
